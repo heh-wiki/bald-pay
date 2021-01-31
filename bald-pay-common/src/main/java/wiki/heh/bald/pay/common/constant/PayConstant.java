09465a74d96079489ebe8449396fc516be0b30a4
Ethan <hehwiki@aliyun.com>
2021-01-31 23:15:53 +0800

    清理代码

--- bald-pay-common/src/main/java/wiki/heh/bald/pay/common/constant/PayConstant.java
+++ bald-pay-common/src/main/java/wiki/heh/bald/pay/common/constant/PayConstant.java
@@ -20,40 +20,41 @@ public class PayConstant {
 	public final static String PAY_CHANNEL_ALIPAY_PC = "ALIPAY_PC";	    		// 支付宝PC支付
 	public final static String PAY_CHANNEL_ALIPAY_WAP = "ALIPAY_WAP";	    	// 支付宝WAP支付
 	public final static String PAY_CHANNEL_ALIPAY_QR = "ALIPAY_QR";	    		// 支付宝当面付之扫码支付
+	public final static String PAY_CHANNEL_ALIPAY_JSAPI = "ALIPAY_JSAPI";	    // 支付宝小程序支付
 
-	public final static String CHANNEL_NAME_WX = "WX"; 				// 渠道名称:微信
-	public final static String CHANNEL_NAME_ALIPAY = "ALIPAY"; 		// 渠道名称:支付宝
+	public final static String CHANNEL_NAME_WX = "WX"; 							// 渠道名称:微信
+	public final static String CHANNEL_NAME_ALIPAY = "ALIPAY"; 					// 渠道名称:支付宝
 
 
 	
-	public final static byte PAY_STATUS_EXPIRED = -2; 	// 订单过期
-	public final static byte PAY_STATUS_FAILED = -1; 	// 支付失败
-	public final static byte PAY_STATUS_INIT = 0; 		// 初始态
-	public final static byte PAY_STATUS_PAYING = 1; 	// 支付中
-	public final static byte PAY_STATUS_SUCCESS = 2; 	// 支付成功
-	public final static byte PAY_STATUS_COMPLETE = 3; 	// 业务完成
+	public final static byte PAY_STATUS_EXPIRED = -2; 			// 订单过期
+	public final static byte PAY_STATUS_FAILED = -1; 			// 支付失败
+	public final static byte PAY_STATUS_INIT = 0; 				// 初始态
+	public final static byte PAY_STATUS_PAYING = 1; 			// 支付中
+	public final static byte PAY_STATUS_SUCCESS = 2; 			// 支付成功
+	public final static byte PAY_STATUS_COMPLETE = 3; 			// 业务完成
 
-	public final static byte TRANS_STATUS_INIT = 0; 		// 初始态
-	public final static byte TRANS_STATUS_TRANING = 1; 		// 转账中
-	public final static byte TRANS_STATUS_SUCCESS = 2; 		// 成功
-	public final static byte TRANS_STATUS_FAIL = 3; 		// 失败
-	public final static byte TRANS_STATUS_COMPLETE = 4; 	// 业务完成
+	public final static byte TRANS_STATUS_INIT = 0; 			// 初始态
+	public final static byte TRANS_STATUS_TRANING = 1; 			// 转账中
+	public final static byte TRANS_STATUS_SUCCESS = 2; 			// 成功
+	public final static byte TRANS_STATUS_FAIL = 3; 			// 失败
+	public final static byte TRANS_STATUS_COMPLETE = 4; 		// 业务完成
 
-	public final static byte TRANS_RESULT_INIT = 0; 		// 不确认结果
-	public final static byte TRANS_RESULT_REFUNDING = 1; 	// 等待手动处理
-	public final static byte TRANS_RESULT_SUCCESS = 2; 		// 确认成功
-	public final static byte TRANS_RESULT_FAIL = 3; 		// 确认失败
+	public final static byte TRANS_RESULT_INIT = 0; 			// 不确认结果
+	public final static byte TRANS_RESULT_REFUNDING = 1; 		// 等待手动处理
+	public final static byte TRANS_RESULT_SUCCESS = 2; 			// 确认成功
+	public final static byte TRANS_RESULT_FAIL = 3; 			// 确认失败
 
-	public final static byte REFUND_STATUS_INIT = 0; 		// 初始态
-	public final static byte REFUND_STATUS_REFUNDING = 1; 	// 转账中
-	public final static byte REFUND_STATUS_SUCCESS = 2; 	// 成功
-	public final static byte REFUND_STATUS_FAIL = 3; 		// 失败
-	public final static byte REFUND_STATUS_COMPLETE = 4; 	// 业务完成
+	public final static byte REFUND_STATUS_INIT = 0; 			// 初始态
+	public final static byte REFUND_STATUS_REFUNDING = 1; 		// 转账中
+	public final static byte REFUND_STATUS_SUCCESS = 2; 		// 成功
+	public final static byte REFUND_STATUS_FAIL = 3; 			// 失败
+	public final static byte REFUND_STATUS_COMPLETE = 4; 		// 业务完成
 
-	public final static byte REFUND_RESULT_INIT = 0; 		// 不确认结果
-	public final static byte REFUND_RESULT_REFUNDING = 1; 	// 等待手动处理
-	public final static byte REFUND_RESULT_SUCCESS = 2; 	// 确认成功
-	public final static byte REFUND_RESULT_FAIL = 3; 		// 确认失败
+	public final static byte REFUND_RESULT_INIT = 0; 			// 不确认结果
+	public final static byte REFUND_RESULT_REFUNDING = 1; 		// 等待手动处理
+	public final static byte REFUND_RESULT_SUCCESS = 2; 		// 确认成功
+	public final static byte REFUND_RESULT_FAIL = 3; 			// 确认失败
 
 	public final static String MCH_NOTIFY_TYPE_PAY = "1";		// 商户通知类型:支付订单
 	public final static String MCH_NOTIFY_TYPE_TRANS = "2";		// 商户通知类型:转账订单
@@ -64,7 +65,7 @@ public class PayConstant {
 	public final static byte MCH_NOTIFY_STATUS_FAIL = 3;		// 通知失败
 
 
-	public final static String RESP_UTF8 = "UTF-8";			// 通知业务系统使用的编码
+	public final static String RESP_UTF8 = "UTF-8";				// 通知业务系统使用的编码
 
 	public static final String RETURN_PARAM_RETCODE = "retCode";
 	public static final String RETURN_PARAM_RETMSG = "retMsg";
