35985bf57f36fda3668ad447d41c5282791dd434
Ethan <hehwiki@aliyun.com>
2021-02-06 17:36:33 +0800

    新增客户端操作商户接口

--- bald-pay-mgr/src/main/java/wiki/heh/bald/pay/mgr/controller/MchInfoController.java
+++ bald-pay-mgr/src/main/java/wiki/heh/bald/pay/mgr/controller/MchInfoController.java
@@ -6,12 +6,14 @@ import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 import wiki.heh.bald.pay.common.util.DateUtil;
 import wiki.heh.bald.pay.mgr.model.MchInfo;
+import wiki.heh.bald.pay.mgr.model.form.MchInfoParam;
 import wiki.heh.bald.pay.mgr.service.MchInfoService;
 import wiki.heh.bald.pay.mgr.util.PageModel;
 
@@ -36,10 +38,10 @@ public class MchInfoController {
     @GetMapping("edit.html")
     public String editInput(String mchId, ModelMap model) {
         MchInfo item = null;
-        if(StringUtils.isNotBlank(mchId)) {
-           item = mchInfoService.selectMchInfo(mchId);
+        if (StringUtils.isNotBlank(mchId)) {
+            item = mchInfoService.selectMchInfo(mchId);
         }
-        if(item == null) item = new MchInfo();
+        if (item == null) item = new MchInfo();
         model.put("item", item);
         return "mch_info/edit";
     }
@@ -49,11 +51,11 @@ public class MchInfoController {
     public String list(@ModelAttribute MchInfo mchInfo, Integer pageIndex, Integer pageSize) {
         PageModel pageModel = new PageModel();
         int count = mchInfoService.count(mchInfo);
-        if(count <= 0) return JSON.toJSONString(pageModel);
-        List<MchInfo> mchInfoList = mchInfoService.getMchInfoList((pageIndex-1)*pageSize, pageSize, mchInfo);
-        if(!CollectionUtils.isEmpty(mchInfoList)) {
+        if (count <= 0) return JSON.toJSONString(pageModel);
+        List<MchInfo> mchInfoList = mchInfoService.getMchInfoList((pageIndex - 1) * pageSize, pageSize, mchInfo);
+        if (!CollectionUtils.isEmpty(mchInfoList)) {
             JSONArray array = new JSONArray();
-            for(MchInfo mi : mchInfoList) {
+            for (MchInfo mi : mchInfoList) {
                 JSONObject object = (JSONObject) JSONObject.toJSON(mi);
                 object.put("createTime", DateUtil.date2Str(mi.getCreateTime()));
                 array.add(object);
@@ -78,25 +80,43 @@ public class MchInfoController {
         mchInfo.setReqKey(po.getString("reqKey"));
         mchInfo.setResKey(po.getString("resKey"));
         int result;
-        if(StringUtils.isBlank(mchId)) {
+        if (StringUtils.isBlank(mchId)) {
             // 添加
             result = mchInfoService.addMchInfo(mchInfo);
-        }else {
+        } else {
             // 修改
             mchInfo.setMchId(mchId);
             result = mchInfoService.updateMchInfo(mchInfo);
         }
         _log.info("保存商户记录,返回:{}", result);
-        return result+"";
+        return result + "";
+    }
+
+    @PostMapping("save")
+    @ResponseBody
+    public String save(@RequestBody MchInfoParam params) {
+        _log.info("请求保存商户记录");
+        MchInfo mchInfo = new MchInfo();
+        BeanUtils.copyProperties(params, mchInfo);
+        int result;
+        if (StringUtils.isBlank(params.getMchId())) {
+            // 添加
+            result = mchInfoService.addMchInfo(mchInfo);
+        } else {
+            // 修改
+            result = mchInfoService.updateMchInfo(mchInfo);
+        }
+        _log.info("保存商户记录,返回:{}", result);
+        return result + "";
     }
 
     @GetMapping("view.html")
     public String viewInput(String mchId, ModelMap model) {
         MchInfo item = null;
-        if(StringUtils.isNotBlank(mchId)) {
+        if (StringUtils.isNotBlank(mchId)) {
             item = mchInfoService.selectMchInfo(mchId);
         }
-        if(item == null) item = new MchInfo();
+        if (item == null) item = new MchInfo();
         model.put("item", item);
         return "mch_info/view";
     }
---
+++ bald-pay-mgr/src/main/java/wiki/heh/bald/pay/mgr/model/form/MchInfoParam.java
@@ -0,0 +1,83 @@
+package wiki.heh.bald.pay.mgr.model.form;
+
+/**
+ * 商户信息参数类
+ *
+ * @author heh
+ * @date 2021/2/5
+ */
+public class MchInfoParam {
+
+    /*商户ID*/
+    private String mchId;
+    /*名称*/
+    private String name;
+    /*类型*/
+    private String type;
+    /*请求私钥*/
+    private String reqKey;
+    /*响应私钥*/
+    private String resKey;
+    /* 商户状态,0-禁用,1-启用*/
+    private Byte state;
+
+    public String getMchId() {
+        return mchId;
+    }
+
+    public void setMchId(String mchId) {
+        this.mchId = mchId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getReqKey() {
+        return reqKey;
+    }
+
+    public void setReqKey(String reqKey) {
+        this.reqKey = reqKey;
+    }
+
+    public String getResKey() {
+        return resKey;
+    }
+
+    public void setResKey(String resKey) {
+        this.resKey = resKey;
+    }
+
+    public Byte getState() {
+        return state;
+    }
+
+    public void setState(Byte state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "MchInfoParam{" +
+                "mchId='" + mchId + '\'' +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", reqKey='" + reqKey + '\'' +
+                ", resKey='" + resKey + '\'' +
+                ", state=" + state +
+                '}';
+    }
+}
